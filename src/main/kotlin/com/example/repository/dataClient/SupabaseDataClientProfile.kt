edfccdd585fb69ff3564ba5336e21b0ee789b67f
Bacya <bacyadaniil@gmail.com>
2023-06-03 16:57:43 +0300

    add some queries

--- src/main/kotlin/com/example/repository/dataClient/SupabaseDataClientProfile.kt
+++ src/main/kotlin/com/example/repository/dataClient/SupabaseDataClientProfile.kt
@@ -4,6 +4,8 @@ import com.example.bonappetitandroid.dto.Profile
 
 interface SupabaseDataClientProfile {
     suspend fun getProfileData(): List<Profile>
+    suspend fun getEmail(): List<Profile>
+    suspend fun getPassword(): List<Profile>
     suspend fun addProfileData()
     suspend fun deleteProfileData()
 }
